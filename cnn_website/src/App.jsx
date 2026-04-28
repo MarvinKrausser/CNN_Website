fd1c1117396555d8cacb5180a083a90f1714b61b
Marvin Kraußer <koro.no.omo@gmail.com>
2026-04-29 01:16:17 +0200

    created cnn website

---
+++ cnn_website/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Menu from './Menu'
+
+function App() {
+
+  return (
+    <>
+      <Menu />
+    </>
+  );
+}
+
+export default App
---
+++ cnn_website/src/Menu.jsx
@@ -0,0 +1,18 @@
+import './Menu.css'
+
+function Menu() {
+    return (
+        <>
+            <nav id="navbar-main">
+                <li className="menu-item">
+                    <a className="link-text">Test</a>
+                </li>
+                <li className="menu-item">
+                    <a className="link-text">Test2</a>
+                </li>
+            </nav>
+        </>
+    );
+}
+
+export default Menu;
