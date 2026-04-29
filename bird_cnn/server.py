a9cb14cf421cd22dcf666e67aceee0a5104cb296
Marvin Kraußer <koro.no.omo@gmail.com>
2026-04-29 14:53:47 +0200

    changed weight loading

--- bird_cnn/server.py
+++ bird_cnn/server.py
@@ -38,7 +38,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = Bird_CNN(c_in=3, c_hidden=15, c_out=7)
 full_path = os.path.join(BUILD_PATH, "bird_cnn")
-model.load_state_dict(torch.load(full_path, weights_only=False))
+model.load_state_dict(torch.load(full_path, map_location=torch.device(device)))
 model.to(device)
 model.eval()
 
